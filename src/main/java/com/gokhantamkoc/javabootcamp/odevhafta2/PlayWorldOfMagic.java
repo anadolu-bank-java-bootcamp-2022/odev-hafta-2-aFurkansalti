1671b4ac42181b081a52c5139103c1c99d3c7504
aFurkansalti <ahmetfurkansalti@gmail.com>
2022-06-14 20:16:50 +0300

    PlayWorldOfMagic.java is modified

--- src/main/java/com/gokhantamkoc/javabootcamp/odevhafta2/PlayWorldOfMagic.java
+++ src/main/java/com/gokhantamkoc/javabootcamp/odevhafta2/PlayWorldOfMagic.java
@@ -44,6 +44,120 @@ public class PlayWorldOfMagic implements CommandLineRunner {
 		int spellsUsed = 0;
 		// ______ BASLANGIC _______ Kodunuz buradan baslamali
 		
+		float health0 = bossHPs[0];
+		while (true) {
+			String randomMagicianSpells = " ";                          // Değerler koşulda atandığı için ilk değer olarak boşluk atadım.
+			float randomSpellDamageInfo = 0;
+
+			double randomValue = Math.random() * 100;					// Diğer büyülere de küçük bir şans vermek istedim.
+			if ((randomValue >= 0) && (randomValue <= 90)) {
+				randomMagicianSpells = magicianSpells[0];
+				randomSpellDamageInfo = spellDamageInfo[0];
+			} else if ((randomValue > 90) && (randomValue <= 97)) {
+				randomMagicianSpells = magicianSpells[1];
+				randomSpellDamageInfo = spellDamageInfo[1];
+			} else if ((randomValue > 97) && (randomValue <= 100)) {
+				randomMagicianSpells = magicianSpells[2];
+				randomSpellDamageInfo = spellDamageInfo[2];
+			}
+
+			health0 -= randomSpellDamageInfo;							// Random olarak gelen büyü kadar haar vermiş olduğumuz satır.
+
+			System.out.println(randomSpellDamageInfo + " damage done to the " + bossNames[0] + " by " + randomMagicianSpells + ".");
+
+			if (health0 <= 0) {											// Sıfırdan küçükse (-) değerlere inmemesi içi bir koşul.
+				System.out.println(bossNames[0] + "'s health is " + 0 + ".");
+			} else {
+				System.out.println(bossNames[0] + "'s health is " + health0 + ".");
+			}
+
+			spellsUsed++;
+
+			if (health0 <= 0.0f) {										// Boss öldüğü zaman döngüden çıktığımız koşul.
+				System.out.println(bossNames[0] + " killed!");
+				break;
+			}
+		}
+
+		System.out.println("**************************************");	// Geri kalan kısım yukarıdakiler ile aynı.
+
+		float health1 = bossHPs[1];
+		while (true) {
+			String randomMagicianSpells = " ";
+			float randomSpellDamageInfo = 0;
+
+			double randomValue = Math.random() * 100;
+			if ((randomValue >= 0) && (randomValue <= 90)) {
+				randomMagicianSpells = magicianSpells[0];
+				randomSpellDamageInfo = spellDamageInfo[0];
+			} else if ((randomValue > 90) && (randomValue <= 97)) {
+				randomMagicianSpells = magicianSpells[1];
+				randomSpellDamageInfo = spellDamageInfo[1];
+			} else if ((randomValue > 97) && (randomValue <= 100)) {
+				randomMagicianSpells = magicianSpells[2];
+				randomSpellDamageInfo = spellDamageInfo[2];
+			}
+
+			health1 -= randomSpellDamageInfo;
+
+			System.out.println(randomSpellDamageInfo + " damage done to the " + bossNames[1] + " by " + randomMagicianSpells + ".");
+			if (health1 <= 0) {
+				System.out.println(bossNames[1] + "'s health is " + 0 + ".");
+			} else {
+				System.out.println(bossNames[1] + "'s health is " + health1 + ".");
+			}
+
+			spellsUsed++;
+
+			if (health1 <= 0.0f) {
+				System.out.println(bossNames[1] + " killed!");
+				break;
+			}
+		}
+
+		System.out.println("**************************************");
+
+		float health2 = bossHPs[2];
+		while (true) {
+			String randomMagicianSpells = " ";
+			float randomSpellDamageInfo = 0;
+
+			double randomValue = Math.random() * 100;
+			if ((randomValue >= 0) && (randomValue <= 90)) {
+				randomMagicianSpells = magicianSpells[0];
+				randomSpellDamageInfo = spellDamageInfo[0];
+			} else if ((randomValue > 90) && (randomValue <= 97)) {
+				randomMagicianSpells = magicianSpells[1];
+				randomSpellDamageInfo = spellDamageInfo[1];
+			} else if ((randomValue > 97) && (randomValue <= 100)) {
+				randomMagicianSpells = magicianSpells[2];
+				randomSpellDamageInfo = spellDamageInfo[2];
+			}
+
+			health2 -= randomSpellDamageInfo;
+
+			System.out.println(randomSpellDamageInfo + " damage done to the " + bossNames[2] + " by " + randomMagicianSpells + ".");
+
+			if (health2 <= 0) {
+				System.out.println(bossNames[2] + "'s health is " + 0 + ".");
+			} else {
+				System.out.println(bossNames[2] + "'s health is " + health2 + ".");
+			}
+
+			spellsUsed++;
+
+			if (health2 <= 0.0f) {
+				System.out.println(bossNames[2] + " killed!");
+				break;
+			}
+		}
+
+		System.out.println("**************************************");
+
+		System.out.println("Number of spells used: " + spellsUsed);
+
+		System.out.println("**************************************");
+
 		// ______ SON _______ Kodunuz burada bitmeli
 		/* NOT: ______ BASLANGIC _______ ve ______ SON _______ 
 		 * arasina istediginiz kadar sayida satir ekleyebilirsiniz.
